2b272bffe31c6937b3f06dd91673b176a352a820
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-03-27 00:11:14 +0100

    build and deploy in one

--- deployhaxe.js
+++ deployhaxe.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 
-fs.copyFileSync('bin/Main.js', 'www/Main.js');
-fs.copyFileSync('bin/Main.wasm', 'www/Main.wasm');
+fs.copyFileSync('bin/Main.js', 'docs/Main.js');
+fs.copyFileSync('bin/Main.wasm', 'docs/Main.wasm');
